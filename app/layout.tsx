ad2f730714e31ca72d0511f64bd629da6f8cf3d4
dvsalmah <dealovasalmah12@gmail.com>
2026-08-21 20:00:00 +0700

    feat: import theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,11 @@ import "./globals.css";
 import { Navbar } from "@/features/layout/components/navbar";
 import { Footer } from "@/features/layout/components/footer";
 import { Geist } from "next/font/google";
-import { cn } from "@/lib/utils";
+import { cn } from "@/shared/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
+
+import { ThemeProvider } from "@/shared/components/theme-provider";
 
 export const metadata: Metadata = {
   title: "Berita Kini - Portal Berita Terpercaya",
@@ -15,19 +17,21 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="id" className={cn("antialiased font-sans bg-background text-foreground scroll-smooth", "font-sans", geist.variable)}>
+    <html lang="id" className={cn("antialiased font-sans bg-background text-foreground scroll-smooth", geist.variable)} suppressHydrationWarning>
       <head>
         <meta name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
       </head>
       <body className="min-h-screen w-full overflow-x-hidden flex flex-col bg-background text-foreground">
-        <Navbar />
-        <main className="flex-1 flex flex-col">
-          <div className="max-w-full md:max-w-7xl mx-auto px-4 py-2 md:p-6">
-            {children}
-          </div>
-        </main>
-        <Footer />
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Navbar />
+          <main className="flex-1 flex flex-col">
+            <div className="max-w-full md:max-w-7xl mx-auto px-4 py-2 md:p-6">
+              {children}
+            </div>
+          </main>
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
